fc86f05af5db73dd5c14a4222850d7dd5166ed30
sskalos4 <smskalos@ucdavis.edu>
2019-10-23 18:03:44 -0700

    new

--- Script/dBBMM_cleaned.R
+++ Script/dBBMM_cleaned.R
@@ -786,5 +786,180 @@ summary(longi)
 summary(lattitude)
 
 
+## Now let's do the above with the other females
+## NOHA 30.1 Bre
+library(move)
+bre_move <- move(x = "~/Desktop/R_Forever/Dissertation/noha-move-hab/Data/NOHA 30.1.csv")
+show(bre_move)
+n.locs(bre_move) # number of locations
+head(timeLag(bre_move, units="mins")) # time difference between locations - this is misleading because her first few days were set to two hour locations, but were then changd to 1 hour locations; make sure to look at ALL time differences
+head(timestamps(bre_move))
+summary(timeLag(bre_move, units="mins"))
+bre_lag <- timeLag(bre_move, units="mins")
+write.csv(bre_lag, file = "bre_lag.csv")
+
+# burst the movestack object to exclude any loactions that are greater than 31.5 minutes apart (because a several locations are 31.5 mins and not 30 mis) - this is to prevent calculations of bridges and motion variance overnight between the last location of the previous day and the first location of the next morning, which are typically 400+ mins
+
+bre_bursted <- move::burst(bre_move, c('normal','long')[1+(timeLag(bre_move, units='mins')>31.5)])
+
+#plot bre's locations
+#par(mfcol=1:2)
+plot(bre_bursted, type="o", col=3, lwd=2, pch=20, xlab="location_long",ylab="location_lat")
+
+#plot bre's locations with ggmap over map layer just to see if it is geographically correct! - it is
+install.packages("ggmap")
+library(ggmap)
+require(ggmap) #these packages are necessary to work with google maps
+#require(mapproj)
+bre_df <- as(bre_bursted, "data.frame")
+m <- get_map(bbox(extent(bre_bursted)*1.1), source="stamen", zoom=12)
+ggmap(m)+geom_path(data=bre_df, aes(x=location.long, y=location.lat))
+
+# transform coordinates from lat lon, center = T is required for the dbbmm to operate properly according to Bart on the movebank help chat
+
+
+bre_bursted_trans <- spTransform(x = bre_bursted, CRSobj = '+proj=utm +zone=10 +datum=NAD83 +units=m +ellps=GRS80 +towgs84=0,0,0 +lon_0=-122.0374075 +lat_0=38.2021575', center = T)
+proj4string(bre_bursted_trans)
+
+str(bre_bursted)
+
+# bring in raster UTM with NAD 83 projection from ArcMAP because above code isn't working
+
+nlcd_utm_cinn <- raster("~/Desktop/R_Forever/RRF/Data/Raster_UTM/NLCD_UTM.tif")
+#plot(nlcd_utm)
+#str(nlcd_utm)
+#as.data.frame(nlcd_utm, xy = TRUE)
+
+#current projection - this is not working - something happens in this proejction transformation and the landcover values are lost for some reason.
+str(nlcd_utm_cinn)
+str(bre_bursted_trans)
+
+Suisun_nlcd_trans_bre <- raster(nlcd_utm_cinn)
+crs(Suisun_nlcd_trans_bre) <- "+proj=utm +zone=10 +datum=NAD83 +units=m +ellps=GRS80 +towgs84=0,0,0 +lon_0=-122.0374075 +lat_0=38.2021575 "
+
+#now they match
+str(bre_bursted_trans)
+str(Suisun_nlcd_trans_bre)
+
+bre_dbbmm <- brownian.bridge.dyn(bre_bursted_trans, burstType = 'normal', raster = Suisun_nlcd_trans_bre, location.error = 10, ext = .3, time.step = 60, margin = 3, window.size = 7) #location error is 10 m as per the transmitter specifications, extent is 30% of raster extent, time step is 60 mins becasue locations were approximately every hour, margin is 3 which is the minimum number of locations needed to calculate breakpoints a a leave-one-out approach, and window size is 7 because this is equivalent to 7 locations, which equals  7 hours and may be able to detect behavioral changes within this relatively short window.
+
+## below are the UDs calculated from the dbbmm
+bre_dbbmm_UD<-new(".UD",calc(bre_dbbmm, sum)) ## it works!!!
+head(bre_dbbmm_UD)
+str(bre_dbbmm_UD)
+summary(bre_dbbmm_UD)
+
+## get the UD raster layer??
+bre_ud <- UDStack(bre_dbbmm)
+summary(bre_ud)
+
+#now plot the UD on the left and the actual movement path on the right
+#I can't figure out how to change the map area such that the map area is zoomed in, but whatever
+par(mfrow=c(1,2))
+plot(bre_dbbmm_UD, xlab="longitude", ylab="latitude")
+#zoom(bre_dbbmm_UD)
+plot(bre_dbbmm_UD, xlab="longitude", ylab="latitude")
+lines(bre_bursted_trans, center=TRUE, col=3, lwd=2)
+#plot(mama_dbbmm, xlab="location_long", ylab="location_lat")
+#points(spTransform(mama_bursted, center=TRUE), col=8)
+
+# this plots the 50% and 95% UD contour lines around the UDs
+plot(bre_dbbmm_UD, xlab="location_long", ylab="location_lat")
+contour(bre_dbbmm_UD, levels=c(.5, .95), col=c(6,2), add=TRUE, lwd=2)
+
+#get the area of the 95% UD - i think these areas are in meters
+bre_cont95 <- getVolumeUD(bre_dbbmm_UD)
+bre_cont95 <- bre_cont95<=.95
+area95 <- sum(values(bre_cont95))
+area95
+
+#get the area of the 50% UD - i think these areas are in meters
+bre_cont5 <- getVolumeUD(bre_dbbmm_UD)
+bre_cont5 <- bre_cont5<=.5
+area5 <- sum(values(bre_cont5))
+area5
+
+## Ok, now let's follow Brian's steps and convert the DBBMM object to a SpatialLineDataFrame
+
+#dbbmm dataframe- keep this!
+bre.dbbmm.df <- as.data.frame(bre_dbbmm_UD, xy = TRUE)
+bre_ud_raster <- rasterFromXYZ(bre.dbbmm.df, crs = "+proj=utm +zone=10 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0", digits = 5 )
+
+## write raster - this is returning an empty raster and we don't know why
+#writeRaster(bre_ud_raster, "C:/Users/sskalos/Documents/noha-move-hab/Output/mama_ud_raster.tif", overwrite = TRUE)
+
+#writeRaster(bre_dbbmm, "C:/Users/sskalos/Documents/noha-move-hab/Output/mama_ud_raster2.tif", overwrite = TRUE)
+
+#writeRaster(bre_ud, "C:/Users/sskalos/Documents/noha-move-hab/Output/lauren_ud_raster3.tif", overwrite = TRUE)
+
+writeRaster(bre_dbbmm_UD, "~/Desktop/R_Forever/Dissertation/noha-move-hab/Output/bre_ud_raster.tif", overwrite = TRUE)
+
+#but it does create the raster appropriately and plots below (weird)
+plot(bre_ud_raster)
+str(bre_ud_raster) # and there are values within this new raster (extra weird)
+
+#save contours to shapefiles - this works
+
+#example from Bart on Movebank - it works
+#require(move)
+#example(brownian.bridge.dyn)
+#cont<-raster2contour(dbbmm, level=c(.5,.95))
+#writeOGR(cont, dsn = '.', layer = 'mycontLines', driver = "ESRI Shapefile")
+
+## try with mama data - it works
+require(move)
+#cont2 <-raster2contour(bre_ud, level=c(.5,.95))
+#writeOGR(cont2, dsn = '.', layer = 'bre_contour2', driver = "ESRI Shapefile")
+
+#works with either the mama_ud raster (above) or the mama_dbbmm_UD raster below, but below seems to be correct as it contains one layer, not every dbbmm step
+require(move)
+cont_bre <-raster2contour(bre_dbbmm_UD, level=c(.5,.95))
+writeOGR(cont_bre, dsn = '.', layer = 'bre_contour', driver = "ESRI Shapefile")
+
+#getwd()
+#ploygon <- readOGR(dsn = "/Users/Shannon/Desktop/R_Forever/Dissertation/noha-move-hab/Output" ,layer = "mama_contour3")
+#mama_WGS <- spTransform(cont3, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")) 
+#writeOGR(cont3, dsn = "mama_contours.kml", layer = "mama_WGS", driver = "KML", overwrite_layer = TRUE)
+
+#using the nlcd_utm raster lines up with the correct number of rows and columns from our dbbmm dataframe, and we can extract the landcover values (finally!)
+nlcd_sp_bre <- SpatialPoints(bre.dbbmm.df[,1:2], proj4string = crs(Suisun_nlcd_trans_bre))
+nlcd_extract_bre <- extract(nlcd_utm, nlcd_sp_bre)
+head(nlcd_extract_bre)
+nlcd_extract_bre[which(!is.na(nlcd_extract_bre))]
+
+#check to make sure the have the same # of columns and rows
+str(Suisun_nlcd_trans_bre)
+str(bre_dbbmm_UD)
+
+# test to make sure it works - it does (red square represents the nlcd raster layer)
+plot(bre_dbbmm_UD)
+library(scales)
+plot(nlcd_utm, col = alpha("red", .5), add = TRUE)
+
+# combine the raster cell probabilities with their coord pairs with landcover grid cells
+final_bre <- cbind.data.frame(bre.dbbmm.df, nlcd_extract_bre)
+head(final_bre)
+
+#above works, but returns all columns, including empty grid cells with NA and 0 values
+# below code removes NA in the 4th column (the landcover column) and returns only columns with landcover values 
+final_bre <- final_bre[which(!is.na(final_bre[,4])),]
+head(final_bre)
+
+# for loop to calculate probabilities of use within each landcover types using the UDs
+prob.vec <- rep(NA, length(unique(final_bre[,4])))
+unique.vec <- unique(final_bre[,4])
+tot.prob <- sum(final_bre[,3])
+for (i in 1:length(prob.vec)){
+  prob.vec[i] <- sum(final_bre[which(final_bre[,4] == unique.vec[i]),3])/tot.prob
+}
+#check that the for loop worked and the probabilities sum to 1 - they do
+sum(prob.vec)
+
+#save the probability table for each landcover class - it works!
+probs.cover.tables <- cbind(prob.vec, unique.vec)
+
+#view the entire table
+probs.cover.tables
+write.csv(probs.cover.tables, file = "bre_landcover_probs_final.csv")
 
 
