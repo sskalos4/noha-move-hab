56ab791a3a6820dc1d280a8a799d417d5f54d524
sskalos4 <smskalos@ucdavis.edu>
2019-10-24 19:25:13 -0700

    NEW

--- Script/dBBMM_cleaned.R
+++ Script/dBBMM_cleaned.R
@@ -87,7 +87,7 @@ plot(Suisun_nlcd_trans)
 
 # From Kranstauber et al: increasing the size of the window increases reliability of the motion variance estimation at the cost of missing short term changes in the variation parameter, and should be close to, but less than, 24. Increasing the size of the margin, in contrast, enhances the power to identify "weak" breakpoints at the cost of not detecting breakpoints within the margin. 
 
-mama_dbbmm <- brownian.bridge.dyn(mama_bursted_trans, burstType = 'normal', raster = Suisun_nlcd_trans, location.error = 10, ext = .3, time.step = 72, margin = 3, window.size = 7) #location error is 10 m as per the transmitter specifications, extent is 30% of raster extent, time step is 60 mins becasue locations were approximately every hour, margin is 3 which is the minimum number of locations needed to calculate breakpoints a a leave-one-out approach, and window size is 7 because this is equivalent to 7 locations, which equals  7 hours and may be able to detect behavioral changes within this relatively short window.
+mama_dbbmm <- brownian.bridge.dyn(mama_bursted_trans, burstType = 'normal', raster = Suisun_nlcd_trans, location.error = 10, ext = .3, time.step = 60, margin = 3, window.size = 7) #location error is 10 m as per the transmitter specifications, extent is 30% of raster extent, time step is 60 mins becasue locations were approximately every hour, margin is 3 which is the minimum number of locations needed to calculate breakpoints a a leave-one-out approach, and window size is 7 because this is equivalent to 7 locations, which equals  7 hours and may be able to detect behavioral changes within this relatively short window.
 
 ## below are the UDs calculated from the dbbmm
 mama_dbbmm_UD<-new(".UD",calc(mama_dbbmm, sum)) ## it works!!!
@@ -131,7 +131,7 @@ area5
 
 #dbbmm dataframe- keep this!
 dbbmm.df <- as.data.frame(mama_dbbmm_UD, xy = TRUE)
-dbbmm.df[which(dbbmm.df[,3] == 0), 3] <- NA # this removes the zeros from the raster file
+#dbbmm.df[which(dbbmm.df[,3] == 0), 3] <- NA # this removes the zeros from the raster file
 mama_ud_raster <- rasterFromXYZ(dbbmm.df, crs = "+proj=utm +zone=10 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0", digits = 5 )
 
 ## write raster - this is returning an empty raster and we don't know why
@@ -168,8 +168,8 @@ writeOGR(cont2, dsn = '.', layer = 'mama_contour2', driver = "ESRI Shapefile")
 
 #works with either the mama_ud raster (above) or the mama_dbbmm_UD raster below, but below seems to be correct as it contains one layer, not every dbbmm step
 require(move)
-cont3 <-raster2contour(mama_dbbmm_UD, level=c(.5,.95))
-writeOGR(cont3, dsn = '.', layer = 'mama_contour3', driver = "ESRI Shapefile")
+cont5 <-raster2contour(mama_dbbmm_UD, level=c(.5,.95))
+writeOGR(cont5, dsn = '.', layer = 'mama_contour5', driver = "ESRI Shapefile")
 
 # saves the contours to a kml file
 install.packages('plotKML')
@@ -840,7 +840,7 @@ crs(Suisun_nlcd_trans_Meg) <- "+proj=utm +zone=10 +datum=NAD83 +units=m +ellps=G
 str(Meg_bursted_trans)
 str(Suisun_nlcd_trans_Meg)
 
-Meg_dbbmm <- brownian.bridge.dyn(Meg_bursted_trans, burstType = 'normal', raster = Suisun_nlcd_trans_Meg, location.error = 10, ext = .3, time.step = 30, margin = 3, window.size = 7) #location error is 10 m as per the transmitter specifications, extent is 30% of raster extent, time step is 30 mins becasue locations were approximately every hour, margin is 3 which is the minimum number of locations needed to calculate Megakpoints a a leave-one-out approach, and window size is 7 because this is equivalent to 7 locations, which equals  7 hours and may be able to detect behavioral changes within this relatively short window.
+Meg_dbbmm <- brownian.bridge.dyn(Meg_bursted_trans, burstType = 'normal', raster = Suisun_nlcd_trans_Meg, location.error = 10, ext = .3, time.step = 30, margin = 3, window.size = 13) #location error is 10 m as per the transmitter specifications, extent is 30% of raster extent, time step is 30 mins becasue locations were approximately every hour, margin is 3 which is the minimum number of locations needed to calculate Megakpoints a a leave-one-out approach, and window size is 13 because this is equivalent to 13 locations, which equals  ~7 hours and may be able to detect behavioral changes within this relatively short window.
 
 ## below are the UDs calculated from the dbbmm
 Meg_dbbmm_UD<-new(".UD",calc(Meg_dbbmm, sum)) ## it works!!!
@@ -965,11 +965,11 @@ write.csv(probs.cover.tables, file = "Meg_landcover_probs_final.csv")
 ## NOHA 627 Red
 
 library(move)
+#note: there were 10 records that were duplicate timestamps with no data because they were SMS messages. If you removed them manually from the .csv file and save, this changes the timestamp column permenantly such that the dates are lost and the move function cannot read the file in. You MUST go to movebank and manually mark each row as an outlier, then redownload the file without these outliers first
 
 getDuplicatedTimestamps(x = "~/Desktop/R_Forever/Dissertation/noha-move-hab/Data/NOHA 627.csv")
 
 Red_move <- move(x = "~/Desktop/R_Forever/Dissertation/noha-move-hab/Data/NOHA 627.csv")
-
 
 show(Red_move)
 n.locs(Red_move) # number of locations
@@ -979,9 +979,9 @@ summary(timeLag(Red_move, units="mins"))
 Red_lag <- timeLag(Red_move, units="mins")
 write.csv(Red_lag, file = "Red_lag.csv")
 
-# burst the movestack object to exclude any loactions that are greater than 32.3 minutes apart (because a several locations are 32.3 mins and not 30 mis) - this is to prevent calculations of bridges and motion variance overnight between the last location of the previous day and the first location of the next morning, which are typically 400+ mins
+# burst the movestack object to exclude any loactions that are greater than 32.3 minutes apart (because a several locations are 31.4 mins and not 30 mis) - this is to prevent calculations of bridges and motion variance overnight between the last location of the previous day and the first location of the next morning, which are typically 400+ mins
 
-Red_bursted <- move::burst(Red_move, c('normal','long')[1+(timeLag(Red_move, units='mins')>32.3)])
+Red_bursted <- move::burst(Red_move, c('normal','long')[1+(timeLag(Red_move, units='mins')>31.4)])
 
 #plot Red's locations
 #par(mfcol=1:2)
@@ -1022,7 +1022,7 @@ crs(Suisun_nlcd_trans_Red) <- "+proj=utm +zone=10 +datum=NAD83 +units=m +ellps=G
 str(Red_bursted_trans)
 str(Suisun_nlcd_trans_Red)
 
-Red_dbbmm <- brownian.bridge.dyn(Red_bursted_trans, burstType = 'normal', raster = Suisun_nlcd_trans_Red, location.error = 10, ext = .3, time.step = 30, margin = 3, window.size = 7) #location error is 10 m as per the transmitter specifications, extent is 30% of raster extent, time step is 30 mins becasue locations were approximately every hour, margin is 3 which is the minimum number of locations needed to calculate Redakpoints a a leave-one-out approach, and window size is 7 because this is equivalent to 7 locations, which equals  7 hours and may be able to detect behavioral changes within this relatively short window.
+Red_dbbmm <- brownian.bridge.dyn(Red_bursted_trans, burstType = 'normal', raster = Suisun_nlcd_trans_Red, location.error = 10, ext = .3, time.step = 30, margin = 3, window.size = 13) #location error is 10 m as per the transmitter specifications, extent is 30% of raster extent, time step is 30 mins becasue locations were approximately every hour, margin is 3 which is the minimum number of locations needed to calculate breakpoints with a leave-one-out approach, and window size is 13 (must be odd) because this is equivalent to 14 locations, which equals ~ 7 hours (30 min locations) and may be able to detect behavioral changes within this relatively short window.
 
 ## below are the UDs calculated from the dbbmm
 Red_dbbmm_UD<-new(".UD",calc(Red_dbbmm, sum)) ## it works!!!
@@ -1143,3 +1143,187 @@ probs.cover.tables <- cbind(prob.vec, unique.vec)
 probs.cover.tables
 write.csv(probs.cover.tables, file = "Red_landcover_probs_final.csv")
 
+## Now let's do the above with the other females
+## NOHA 628 Salty
+
+library(move)
+
+### NEED TO MARK OUTLIERS IN MOVEBANK FIRST
+
+#Salty_move <- move(x = "~/Desktop/R_Forever/Dissertation/noha-move-hab/Data/NOHA 628.csv")
+
+df <- read.csv(x = "~/Desktop/R_Forever/Dissertation/noha-move-hab/Data/NOHA 628.csv")
+
+Salty_move <- move(x = "~/Desktop/R_Forever/Dissertation/noha-move-hab/Data/NOHA 628.csv", df$location.long, df$location.lat, time=as.POSIXct(df$timestamp,format="%m/%d/%Y %H:%M"))
+
+show(Salty_move)
+n.locs(Salty_move) # number of locations
+head(timeLag(Salty_move, units="mins")) # time difference between locations - this is misleading because her first few days were set to two hour locations, but were then changd to 1 hour locations; make sure to look at ALL time differences
+head(timestamps(Salty_move))
+summary(timeLag(Salty_move, units="mins"))
+Salty_lag <- timeLag(Salty_move, units="mins")
+write.csv(Salty_lag, file = "Salty_lag.csv")
+
+# burst the movestack object to exclude any loactions that are greater than 32.3 minutes apart (because a several locations are 32.3 mins and not 30 mis) - this is to prevent calculations of bridges and motion variance overnight between the last location of the previous day and the first location of the next morning, which are typically 400+ mins
+
+Salty_bursted <- move::burst(Salty_move, c('normal','long')[1+(timeLag(Salty_move, units='mins')>32.3)])
+
+#plot Salty's locations
+#par(mfcol=1:2)
+plot(Salty_bursted, type="o", col=3, lwd=2, pch=20, xlab="location_long",ylab="location_lat")
+
+#plot Salty's locations with ggmap over map layer just to see if it is geographically correct! - it is
+install.packages("ggmap")
+library(ggmap)
+require(ggmap) #these packages are necessary to work with google maps
+#require(mapproj)
+Salty_df <- as(Salty_bursted, "data.frame")
+m <- get_map(bbox(extent(Salty_bursted)*1.1), source="stamen", zoom=12)
+ggmap(m)+geom_path(data=Salty_df, aes(x=location.long, y=location.lat))
+
+# transform coordinates from lat lon, center = T is requiSalty for the dbbmm to operate properly according to Bart on the movebank help chat
+
+
+Salty_bursted_trans <- spTransform(x = Salty_bursted, CRSobj = '+proj=utm +zone=10 +datum=NAD83 +units=m +ellps=GRS80 +towgs84=0,0,0 +lon_0=-122.0374075 +lat_0=38.2021575', center = T)
+proj4string(Salty_bursted_trans)
+
+str(Salty_bursted)
+
+# bring in raster UTM with NAD 83 projection from ArcMAP because above code isn't working
+
+nlcd_utm_Salty <- raster("~/Desktop/R_Forever/RRF/Data/Raster_UTM/NLCD_UTM.tif")
+#plot(nlcd_utm)
+#str(nlcd_utm)
+#as.data.frame(nlcd_utm, xy = TRUE)
+
+#current projection - this is not working - something happens in this proejction transformation and the landcover values are lost for some reason.
+str(nlcd_utm_Salty)
+str(Salty_bursted_trans)
+
+Suisun_nlcd_trans_Salty <- raster(nlcd_utm_Salty)
+crs(Suisun_nlcd_trans_Salty) <- "+proj=utm +zone=10 +datum=NAD83 +units=m +ellps=GRS80 +towgs84=0,0,0 +lon_0=-122.0374075 +lat_0=38.2021575 "
+
+#now they match
+str(Salty_bursted_trans)
+str(Suisun_nlcd_trans_Salty)
+
+Salty_dbbmm <- brownian.bridge.dyn(Salty_bursted_trans, burstType = 'normal', raster = Suisun_nlcd_trans_Salty, location.error = 10, ext = .3, time.step = 30, margin = 3, window.size = 7) #location error is 10 m as per the transmitter specifications, extent is 30% of raster extent, time step is 30 mins becasue locations were approximately every hour, margin is 3 which is the minimum number of locations needed to calculate Saltyakpoints a a leave-one-out approach, and window size is 7 because this is equivalent to 7 locations, which equals  7 hours and may be able to detect behavioral changes within this relatively short window.
+
+## below are the UDs calculated from the dbbmm
+Salty_dbbmm_UD<-new(".UD",calc(Salty_dbbmm, sum)) ## it works!!!
+head(Salty_dbbmm_UD)
+str(Salty_dbbmm_UD)
+summary(Salty_dbbmm_UD)
+
+## get the UD raster layer?? 
+#Salty_ud <- UDStack(Salty_dbbmm)
+#summary(Salty_ud)
+
+#now plot the UD on the left and the actual movement path on the right
+#I can't figure out how to change the map area such that the map area is zoomed in, but whatever
+par(mfrow=c(1,2))
+plot(Salty_dbbmm_UD, xlab="longitude", ylab="latitude")
+#zoom(Salty_dbbmm_UD)
+plot(Salty_dbbmm_UD, xlab="longitude", ylab="latitude")
+lines(Salty_bursted_trans, center=TRUE, col=3, lwd=2)
+#plot(mama_dbbmm, xlab="location_long", ylab="location_lat")
+#points(spTransform(mama_bursted, center=TRUE), col=8)
+
+# this plots the 50% and 95% UD contour lines around the UDs
+plot(Salty_dbbmm_UD, xlab="location_long", ylab="location_lat")
+contour(Salty_dbbmm_UD, levels=c(.5, .95), col=c(6,2), add=TRUE, lwd=2)
+
+#get the area of the 95% UD - i think these areas are in meters
+Salty_cont95 <- getVolumeUD(Salty_dbbmm_UD)
+Salty_cont95 <- Salty_cont95<=.95
+area95 <- sum(values(Salty_cont95))
+area95
+
+#get the area of the 50% UD - i think these areas are in meters
+Salty_cont5 <- getVolumeUD(Salty_dbbmm_UD)
+Salty_cont5 <- Salty_cont5<=.5
+area5 <- sum(values(Salty_cont5))
+area5
+
+## Ok, now let's follow Brian's steps and convert the DBBMM object to a SpatialLineDataFrame
+
+#dbbmm dataframe- keep this!
+Salty.dbbmm.df <- as.data.frame(Salty_dbbmm_UD, xy = TRUE)
+Salty_ud_raster <- rasterFromXYZ(Salty.dbbmm.df, crs = "+proj=utm +zone=10 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0", digits = 5 )
+
+## write raster - this is returning an empty raster and we don't know why
+#writeRaster(Salty_ud_raster, "C:/Users/sskalos/Documents/noha-move-hab/Output/mama_ud_raster.tif", overwrite = TRUE)
+
+#writeRaster(Salty_dbbmm, "C:/Users/sskalos/Documents/noha-move-hab/Output/mama_ud_raster2.tif", overwrite = TRUE)
+
+#writeRaster(Salty_ud, "C:/Users/sskalos/Documents/noha-move-hab/Output/lauren_ud_raster3.tif", overwrite = TRUE)
+
+writeRaster(Salty_dbbmm_UD, "~/Desktop/R_Forever/Dissertation/noha-move-hab/Output/Salty_ud_raster.tif", overwrite = TRUE)
+
+#but it does create the raster appropriately and plots below (weird)
+plot(Salty_ud_raster)
+str(Salty_ud_raster) # and there are values within this new raster (extra weird)
+
+#save contours to shapefiles - this works
+
+#example from Bart on Movebank - it works
+#require(move)
+#example(brownian.bridge.dyn)
+#cont<-raster2contour(dbbmm, level=c(.5,.95))
+#writeOGR(cont, dsn = '.', layer = 'mycontLines', driver = "ESRI Shapefile")
+
+## try with mama data - it works
+require(move)
+#cont2 <-raster2contour(Salty_ud, level=c(.5,.95))
+#writeOGR(cont2, dsn = '.', layer = 'Salty_contour2', driver = "ESRI Shapefile")
+
+#works with either the mama_ud raster (above) or the mama_dbbmm_UD raster below, but below seems to be correct as it contains one layer, not every dbbmm step
+require(move)
+cont_Salty <-raster2contour(Salty_dbbmm_UD, level=c(.5,.95))
+writeOGR(cont_Salty, dsn = '.', layer = 'Salty_contour', driver = "ESRI Shapefile")
+
+#getwd()
+#ploygon <- readOGR(dsn = "/Users/Shannon/Desktop/R_Forever/Dissertation/noha-move-hab/Output" ,layer = "mama_contour3")
+#mama_WGS <- spTransform(cont3, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")) 
+#writeOGR(cont3, dsn = "mama_contours.kml", layer = "mama_WGS", driver = "KML", overwrite_layer = TRUE)
+
+#using the nlcd_utm raster lines up with the correct number of rows and columns from our dbbmm dataframe, and we can extract the landcover values (finally!)
+nlcd_sp_Salty <- SpatialPoints(Salty.dbbmm.df[,1:2], proj4string = crs(Suisun_nlcd_trans_Salty))
+nlcd_extract_Salty <- extract(nlcd_utm_Salty, nlcd_sp_Salty)
+head(nlcd_extract_Salty)
+nlcd_extract_Salty[which(!is.na(nlcd_extract_Salty))]
+
+#check to make sure the have the same # of columns and rows
+str(Suisun_nlcd_trans_Salty)
+str(Salty_dbbmm_UD)
+
+# test to make sure it works - it does (Salty square represents the nlcd raster layer)
+plot(Salty_dbbmm_UD)
+library(scales)
+plot(nlcd_utm_Salty, col = alpha("Salty", .5), add = TRUE)
+
+# combine the raster cell probabilities with their coord pairs with landcover grid cells
+final_Salty <- cbind.data.frame(Salty.dbbmm.df, nlcd_extract_Salty)
+head(final_Salty)
+
+#above works, but returns all columns, including empty grid cells with NA and 0 values
+# below code removes NA in the 4th column (the landcover column) and returns only columns with landcover values 
+final_Salty <- final_Salty[which(!is.na(final_Salty[,4])),]
+head(final_Salty)
+
+# for loop to calculate probabilities of use within each landcover types using the UDs
+prob.vec <- rep(NA, length(unique(final_Salty[,4])))
+unique.vec <- unique(final_Salty[,4])
+tot.prob <- sum(final_Salty[,3])
+for (i in 1:length(prob.vec)){
+  prob.vec[i] <- sum(final_Salty[which(final_Salty[,4] == unique.vec[i]),3])/tot.prob
+}
+#check that the for loop worked and the probabilities sum to 1 - they do
+sum(prob.vec)
+
+#save the probability table for each landcover class - it works!
+probs.cover.tables <- cbind(prob.vec, unique.vec)
+
+#view the entire table
+probs.cover.tables
+write.csv(probs.cover.tables, file = "Salty_landcover_probs_final.csv")
